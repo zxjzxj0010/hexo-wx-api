78e0730d3edc18a70d669f9f8d675bf82581f591
Zhong Xingjian <zxjzxj0010@126.com>
2026-03-18 17:56:45 +0800

    Update index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -181,38 +181,37 @@ Page({
     })
   },
 
-  // },
-  // //首页图标跳转
-  // onNavRedirect: function (e) {
-  //   var redicttype = e.currentTarget.dataset.redicttype;
-  //   var url = e.currentTarget.dataset.url == null ? '' : e.currentTarget.dataset.url;
-  //   var appid = e.currentTarget.dataset.appid == null ? '' : e.currentTarget.dataset.appid;
-  //   var extraData = e.currentTarget.dataset.extraData == null ? '' : e.currentTarget.dataset.extraData;
-  //   if (redicttype == 'apppage') { //跳转到小程序内部页面         
-  //     wx.navigateTo({
-  //       url: url
-  //     })
-  //   } else if (redicttype == 'webpage') //跳转到web-view内嵌的页面
-  //   {
-  //     url = '../webpage/webpage?url=' + url;
-  //     wx.navigateTo({
-  //       url: url
-  //     })
-  //   } else if (redicttype == 'miniapp') //跳转到其他app
-  //   {
-  //     wx.navigateToMiniProgram({
-  //       appId: appid,
-  //       envVersion: 'release',
-  //       path: url,
-  //       extraData: extraData,
-  //       success(res) {
-  //         // 打开成功
-  //       },
-  //       fail: function (res) {
-  //         console.log(res);
-  //       }
-  //     })
-  //   }
+  //首页图标跳转
+  onNavRedirect: function (e) {
+    var redicttype = e.currentTarget.dataset.redicttype;
+    var url = e.currentTarget.dataset.url == null ? '' : e.currentTarget.dataset.url;
+    var appid = e.currentTarget.dataset.appid == null ? '' : e.currentTarget.dataset.appid;
+    var extraData = e.currentTarget.dataset.extraData == null ? '' : e.currentTarget.dataset.extraData;
+    if (redicttype == 'apppage') { //跳转到小程序内部页面         
+      wx.navigateTo({
+        url: url
+      })
+    } else if (redicttype == 'webpage') //跳转到web-view内嵌的页面
+    {
+      url = '../webpage/webpage?url=' + url;
+      wx.navigateTo({
+        url: url
+      })
+    } else if (redicttype == 'miniapp') //跳转到其他app
+    {
+      wx.navigateToMiniProgram({
+        appId: appid,
+        envVersion: 'release',
+        path: url,
+        extraData: extraData,
+        success(res) {
+          // 打开成功
+        },
+        fail: function (res) {
+          console.log(res);
+        }
+      })
+    }
 
-  // }
+  },
 })
